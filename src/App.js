42b9bc1c49ba3c38856ffbb249a210c42ebbdb7c
Hoybaby <mbartek436@gmail.com>
2021-01-08 00:25:38 -0500

    made my first succesfull component after several troubleshoots. Make sure to make the component first letter capitalize always

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TodoComponent from './components/Title/Title'
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoComponent/>
     
     </div>
   );
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const TodoComponent = () => {
+    return ( 
+        <h1>helloooo!!!</h1>
+     );
+}
+ 
+export default TodoComponent;
